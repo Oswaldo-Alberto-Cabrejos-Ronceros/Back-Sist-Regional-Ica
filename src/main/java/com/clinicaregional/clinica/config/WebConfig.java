35b9ea898de9e0bfed21f5497dbc7feae29c353f
Oswaldo <U22209742@utp.edu.pe>
2025-04-18 17:48:08 -0500

    feat:Configuracion de Cors en WebConfig

---
+++ src/main/java/com/clinicaregional/clinica/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.clinicaregional.clinica.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        //activamos cors para todos los endpoints
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173","https://localhost:5173") //origines permitidos
+                .allowedMethods("GET","POST","PUT","DELETE") //metodos permitidos
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}

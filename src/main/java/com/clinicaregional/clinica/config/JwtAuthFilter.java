0d94eeb7005b86ab89488c3749ee396aa2eae7c5
Oswaldo <U22209742@utp.edu.pe>
2025-04-18 15:55:10 -0500

    feat:Creacion de JwtUtil
    Funcionalidades referentes a autenticacion con JWT

---
+++ src/main/java/com/clinicaregional/clinica/config/JwtAuthFilter.java
@@ -0,0 +1,29 @@
+package com.clinicaregional.clinica.config;
+
+import com.clinicaregional.clinica.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    //inyectamos por constructor
+    private final JwtUtil jwtUtil;
+    @Autowired
+    public JwtAuthFilter(JwtUtil jwtUtil){
+        this.jwtUtil=jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+
+    }
+}

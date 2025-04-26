73831958291e551b818b187e404ac4c906d1fe70
Oswaldo <U22209742@utp.edu.pe>
2025-04-25 20:00:15 -0500

    feat:Desarrollo de TipoDocumentDTO y TipoDocumentoMapper

---
+++ src/main/java/com/clinicaregional/clinica/dto/TipoDocumentoDTO.java
@@ -0,0 +1,24 @@
+package com.clinicaregional.clinica.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TipoDocumentoDTO {
+    private Long id;
+
+    @NotBlank(message = "Nombre es obligatorio")
+    @Size(max=32, message = "El nombre debe tener menos de 32 caracteres")
+    private String nombre;
+
+    @NotBlank(message = "Descripcion es obligatorio")
+    @Size(max=255, message = "La descripcion debe tener menos de 255 caracteres")
+    private String descripcion;
+}

4514833f706eca46a96ce02324fb4dedf925d0ca
Oswaldo <U22209742@utp.edu.pe>
2025-04-25 22:14:47 -0500

    feat:Creacion de PacienteReposity

---
+++ src/main/java/com/clinicaregional/clinica/repository/PacienteRepository.java
@@ -0,0 +1,11 @@
+package com.clinicaregional.clinica.repository;
+
+import com.clinicaregional.clinica.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Optional<Paciente> findByNumeroIdentificacion(String numeroIdentificacion);
+    Optional<Paciente> findByIdAndEstadoIsTrue(Long id);
+}

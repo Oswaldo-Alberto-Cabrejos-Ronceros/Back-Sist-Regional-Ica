d02bc0bb82888f197f14a381d6efb17f4ec75edd
Oswaldo <U22209742@utp.edu.pe>
2025-05-01 13:12:16 -0500

    feat:Creación de la entidad SeguroCobertura

---
+++ src/main/java/com/clinicaregional/clinica/entity/SeguroCobertura.java
@@ -0,0 +1,28 @@
+package com.clinicaregional.clinica.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Filter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "seguro_coberturas")
+//para filtro
+@Filter(name = "estadoActivo", condition = "estado = :estado")
+public class SeguroCobertura extends EntidadConEstado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Seguro seguro;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Cobertura cobertura;
+}

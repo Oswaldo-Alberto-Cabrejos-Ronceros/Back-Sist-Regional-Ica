714ddab022560878fb1a3b7e94b3cba5ca6352f2
Adrián Vega <adrian.vegaf14@gmail.com>
2025-05-02 17:07:19 -0500

    refactor(repository): reemplazar uso de estado por visible en consulta de opiniones activas

--- src/main/java/com/clinicaregional/clinica/repository/OpinionRepository.java
+++ src/main/java/com/clinicaregional/clinica/repository/OpinionRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 @Repository
 public interface OpinionRepository extends JpaRepository<Opinion, Long> {
 
-    Optional<Opinion> findByIdAndEstadoIsTrue(Long id);
-
+    Optional<Opinion> findByIdAndVisibleIsTrue(Long id);
 }

73b8327f773a6d380798c76938016d5d80992e2a
Adrián Vega <adrian.vegaf14@gmail.com>
2025-05-07 21:22:44 -0500

    feat(service): Definir la interfaz de servicio para gestionar operaciones CRUD de las citas

--- src/main/java/com/clinicaregional/clinica/service/CitaService.java
+++ src/main/java/com/clinicaregional/clinica/service/CitaService.java
@@ -2,15 +2,20 @@ package com.clinicaregional.clinica.service;
 
 import com.clinicaregional.clinica.dto.request.CitaRequest;
 import com.clinicaregional.clinica.dto.response.CitaResponse;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface CitaService {
 
     List<CitaResponse> listar();
+
     Optional<CitaResponse> obtenerPorId(Long id);
+
     CitaResponse guardar(CitaRequest citaRequest);
+
     CitaResponse actualizar(Long id, CitaRequest citaRequest);
+
     void eliminar(Long id);
 
 }

6a651ddcc9119c599927adcadf496218901b6f02
CarlosHuamanM <thecarlox2000@gmail.com>
2025-06-01 20:51:00 -0500

    feat:Creacion Entidad Disponibilidad y enum para los dias de la semana

---
+++ src/main/java/com/clinicaregional/clinica/entity/Disponibilidad.java
@@ -0,0 +1,43 @@
+package com.clinicaregional.clinica.entity;
+
+import jakarta.persistence.Entity;
+
+import java.time.LocalTime;
+
+import org.hibernate.annotations.Filter;
+import org.springframework.cglib.core.Local;
+
+import com.clinicaregional.clinica.enums.DiaSemana;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "disponibilidades")
+@Filter(name = "estadoActivo", condition = "estado = :estado")
+public class Disponibilidad {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "disponibilidad_id")
+    private Long id;
+
+    private DiaSemana diaSemana;
+    
+    private LocalTime horaInicio;
+
+    private LocalTime horaFin;
+
+    private String notas;
+
+    @OneToMany
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+}
---
+++ src/main/java/com/clinicaregional/clinica/enums/DiaSemana.java
@@ -0,0 +1,5 @@
+package com.clinicaregional.clinica.enums;
+
+public enum DiaSemana {
+    LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
+}

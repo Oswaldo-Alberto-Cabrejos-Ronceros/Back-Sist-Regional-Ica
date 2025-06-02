529759ddf88510e489b132cfa5131590bd29a545
CarlosHuamanM <thecarlox2000@gmail.com>
2025-06-01 21:20:08 -0500

    feat: Creacion del DTO response para la entidad Disponibilidad

---
+++ src/main/java/com/clinicaregional/clinica/dto/response/DisponibilidadResponse.java
@@ -0,0 +1,24 @@
+package com.clinicaregional.clinica.dto.response;
+
+import java.time.LocalTime;
+
+import com.clinicaregional.clinica.enums.DiaSemana;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DisponibilidadResponse {
+    private Long id;
+    private DiaSemana diaSemana;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+    private String notas;
+    private String nombreMedico;
+
+}

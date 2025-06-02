c5d3d07819e18b9d7f9f005453654d67659fb84c
Oswaldo <U22209742@utp.edu.pe>
2025-06-02 10:23:12 -0500

    feat:Desarrollo de los metodo obtenerHorariosBloques y crearHorarioBloque en HorarioBloqueServiceImpl

--- src/main/java/com/clinicaregional/clinica/service/HorarioBloqueService.java
+++ src/main/java/com/clinicaregional/clinica/service/HorarioBloqueService.java
@@ -1,16 +1,16 @@
 package com.clinicaregional.clinica.service;
 
+import com.clinicaregional.clinica.dto.request.HorarioBloqueRequest;
 import com.clinicaregional.clinica.dto.response.HorarioBloqueResponse;
 import com.clinicaregional.clinica.entity.HorarioBloque;
 import com.clinicaregional.clinica.enums.EstadoBloque;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface HorarioBloqueService {
     List<HorarioBloqueResponse> obtenerHorariosBloques();
-    HorarioBloqueResponse crearHorarioBloque(HorarioBloque horarioBloque);
-    Optional<HorarioBloqueResponse> obtenerHorarioBloquePorId(Long id);
+    HorarioBloqueResponse crearHorarioBloque(HorarioBloqueRequest horarioBloqueRequest);
+    HorarioBloqueResponse obtenerHorarioBloquePorId(Long id);
     boolean estaDiponibleHorarioBloque(Long id);
     HorarioBloqueResponse actualizarEstadoHorarioBloque(Long id, EstadoBloque estado);
     void liberarHorarioBloque(Long id);

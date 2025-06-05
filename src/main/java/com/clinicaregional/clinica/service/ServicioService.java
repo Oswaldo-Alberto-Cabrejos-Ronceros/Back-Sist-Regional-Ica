6737e000a3b8570f343ca3743187d7abedbdc467
Adrián Vega <adrian.vegaf14@gmail.com>
2025-06-04 21:06:17 -0500

    feat: Implementación de la paginación para listar pacientes y servicios

--- src/main/java/com/clinicaregional/clinica/service/ServicioService.java
+++ src/main/java/com/clinicaregional/clinica/service/ServicioService.java
@@ -3,6 +3,8 @@ package com.clinicaregional.clinica.service;
 import com.clinicaregional.clinica.dto.request.ServicioRequest;
 import com.clinicaregional.clinica.dto.response.ServicioResponse;
 import com.clinicaregional.clinica.entity.Servicio;
+import com.clinicaregional.clinica.dto.response.PagedResponse;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 public interface ServicioService {
@@ -16,5 +18,7 @@ public interface ServicioService {
     void eliminarServicio(Long id);
 
     ServicioResponse actualizarServicio(Long id, ServicioRequest servicioRequest);
+
+    PagedResponse<ServicioResponse> obtenerServiciosPaginado(Pageable pageable);
     
 }

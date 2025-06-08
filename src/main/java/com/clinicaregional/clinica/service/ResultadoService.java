32df5d54f74d3d379e104f6f6dc1a7afdc83270f
CarlosHuamanM <thecarlox2000@gmail.com>
2025-06-08 16:57:33 -0500

    feat: Crear interfaz del Service para Resultado

---
+++ src/main/java/com/clinicaregional/clinica/service/ResultadoService.java
@@ -0,0 +1,21 @@
+package com.clinicaregional.clinica.service;
+import com.clinicaregional.clinica.dto.request.ResultadoRequest;
+import com.clinicaregional.clinica.dto.response.ResultadoResponse;
+import java.util.List;
+
+
+
+public interface ResultadoService {
+
+    ResultadoResponse crear(ResultadoRequest resultadoRequest);
+
+    List<ResultadoResponse> obtenerPorCita(Long id);
+
+    List<ResultadoResponse> listarPorHistorialClinico(Long citaId);
+
+    ResultadoResponse actualizar(Long id, ResultadoRequest resultadoRequest);
+
+    void eliminar(Long id);
+
+    
+}

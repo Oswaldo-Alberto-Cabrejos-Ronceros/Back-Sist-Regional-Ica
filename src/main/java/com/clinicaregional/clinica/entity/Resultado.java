a0c49f038505f0d60c680f7bf39464bbd4db0969
Oswaldo <U22209742@utp.edu.pe>
2025-06-07 21:45:42 -0500

    feat:Creacion de Dtos y Mapper para Resultado

--- src/main/java/com/clinicaregional/clinica/entity/Resultado.java
+++ src/main/java/com/clinicaregional/clinica/entity/Resultado.java
@@ -1,10 +1,7 @@
 package com.clinicaregional.clinica.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
@@ -12,6 +9,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 @Table(name = "resultados")
+@Builder
 public class Resultado extends EntidadConEstado {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

c62b83f5261a8ed5b0774d29bdfb62a5cf367ce8
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-07-22 12:41:20 -0500

    fix:Cambio en la estadistia del panel de admin, ahora el conteo de ingresos solo tiene en cuenta las citas en estado Atendidas

--- src/main/java/com/clinicaregional/clinica/repository/CitaRepository.java
+++ src/main/java/com/clinicaregional/clinica/repository/CitaRepository.java
@@ -63,7 +63,7 @@ public interface CitaRepository extends JpaRepository<Cita, Long> {
 
     long countByFecha(LocalDate fecha);
 
-    @Query("SELECT SUM(c.servicio.price) FROM Cita c WHERE MONTH(c.fecha) = :mes AND YEAR(c.fecha)= :year")
+    @Query("SELECT SUM(c.servicio.price) FROM Cita c WHERE MONTH(c.fecha) = :mes AND YEAR(c.fecha)= :year AND c.estadoCita = com.clinicaregional.clinica.enums.EstadoCita.ATENDIDA")
     Double sumarMontoServicioPorMes(@Param("mes") int mes, @Param("year") int year);
 
     List<Cita> findAllByFecha(LocalDate fecha);
